fe81ee51264f524acb89da8921b1b088cf13a73e
officialpirat <149113640+officialpirat@users.noreply.github.com>
2025-02-16 19:47:20 +0100

    Update dmopc15c7p2.py

--- 01.Getting-Started/dmopc15c7p2.py
+++ 01.Getting-Started/dmopc15c7p2.py
@@ -1,3 +1,8 @@
+# Ressources : 0,134s, 9,80 Mio
+# Temps d'exécution maximum d'un seul cas : 0,027s
+# Score final : 100/100 (3.0/3 points) 
+# Link : "https://dmoj.ca/problem/dmopc15c7p2"
+
 line = input()
 count_words = line.count(' ') +1
 print(count_words)
